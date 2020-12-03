183d7e3161d4c4ce8ddc6f1dacc7877753702553
Devin Clarke <devinlclarke@gmail.com>
2020-12-03 12:32:07 -0500

    People class initialize method

--- lib/star_wars/people.rb
+++ lib/star_wars/people.rb
@@ -1,10 +1,22 @@
 class People
 
+    attr_accessor :name, :height, :eye_color, :gender
+
     @@all = []
+
+    def initialize(people_data)
+        @name = people_data["name"]
+        @height = people_data["height"]
+        @eye_color = people_data["eyes"]
+        @gender = people_data["eye color"]
+
+
 
     def save
         self.class.all << self
     end
+
+
     def self.all
         @@all
     end
