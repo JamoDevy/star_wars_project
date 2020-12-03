464564bf3aefef7a97d36cdd765df35f3d63fd4d
Devin Clarke <devinlclarke@gmail.com>
2020-12-03 12:45:06 -0500

    Promppt methods for CLI class

--- lib/star_wars/cli.rb
+++ lib/star_wars/cli.rb
@@ -1,3 +1,21 @@
 class CLI
 
+    def welcome
+        puts
+        puts "---Welcome to a galaxy far far away!---".colorize(:blue)
+        puts
+    end
+
+    def main_menu
+        puts "What would you like to search for?".colorize(:green)
+        puts "1. Search for People" 
+        puts "2. Search for Planets"
+        puts "3. Search for Starships"
+    end
+
+end
+
+
+
+
 end
