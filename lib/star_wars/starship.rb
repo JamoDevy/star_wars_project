08dc9e05f0b5a0e69bfd41abbffe44f242bf7919
Devin Clarke <devinlclarke@gmail.com>
2020-12-03 13:26:10 -0500

    Added initialize method to starship class

--- lib/star_wars/starship.rb
+++ lib/star_wars/starship.rb
@@ -1,3 +1,22 @@
 class Starship
 
+    attr_reader :name, :manufacturer, :cost_in_credits, :passengers
+
+    @@all =[]
+
+    def initialize(starship_data)
+        @name = starship_data["name"]
+        @manufacturer = starship_data["manufacturer"]
+        @cost_in_credits = starship_data["cost_in_credits"]
+        @passengers = starship_data["passengers"]
+        save
+    end
+
+    def save
+        self.class.all << self
+    end
+
+    def self.all 
+        @@all
+    end
 end
